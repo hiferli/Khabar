cbe286b7d3ac93dba69790299f252abccfe99349
IshaanJoshi <joshi.ishaan.2001@gmail.com>
2022-12-18 21:45:21 +0530

    Changes in NewsItems

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
     render() {
-        let {newsTitle , newsDescription} = this.props;
+        let {newsTitle , newsDescription , newsImageURL , newsURL} = this.props;
         return (
-            <div>
+            <div className='my-3'>
                 <div className="card" style={{width: "18rem"}}>
-                    <img src="https://www.aljazeera.com/wp-content/uploads/2022/12/2022-12-09T200724Z_1513593204_UP1EIC91JWAH0_RTRMADP_3_SOCCER-WORLDCUP-NLD-ARG-REPORT.jpg?resize=1920%2C1440" className="card-img-top" alt="..." />
+                    <img src={newsImageURL} className="card-img-top" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">{newsTitle}</h5>
                             <p className="card-text">{newsDescription}</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                            <a href="/dummy" className="btn btn-sm btn-primary">Go somewhere</a>
                         </div>
                 </div>
             </div>
