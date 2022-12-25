f5cbf1e61cd6ce1ac7a4220ed86234aceb303912
IshaanJoshi <joshi.ishaan.2001@gmail.com>
2022-12-25 20:42:07 +0530

    Refactor Spinner to FBC

--- src/Components/Spinner.js
+++ src/Components/Spinner.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import Loading from './Loading.gif'
 
-export default class Spinner extends Component {
-    render() {
-        return (
-            <div className='text-center'>
-                <img className='my-3' src={Loading} alt="Loading News!" />
-            </div>
-        )
-    }
+const Spinner = () => {
+    return (
+        <div className='text-center'>
+            <img className='my-3' src={Loading} alt="Loading News!" />
+        </div>
+    )
 }
+
+export default Spinner
